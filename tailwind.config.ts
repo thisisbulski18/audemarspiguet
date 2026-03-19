70dc3eb690774c74a34e8ab5aaf1d51700389f67
thisisbulski18 <wkdbca1881@gmail.com>
2026-03-19 11:44:53 +0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'ap-gold': '#a39263',
+        'ap-black': '#111111',
+      },
+      fontFamily: {
+        serif: ['var(--font-serif)', 'serif'],
+        sans: ['var(--font-sans)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
